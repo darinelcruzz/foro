02837532145696d04bab1d4bce21311e31e76250
Darinel Cruz <darinelcruzz@gmail.com>
2017-02-17 16:08:25 -0600

    Verificar features ya desarrollados usando pruebas de regresión en Laravel

--- tests/features/ShowPostTest.php
+++ tests/features/ShowPostTest.php
@@ -23,9 +23,50 @@ class ShowPostTest extends FeatureTestCase
       $user->posts()->save($post);
 
       // When
-      $this->visit(route('posts.show', $post))
+      $this->visit($post->url)
           ->seeInElement('h1', $post->title)
           ->see($post->content)
           ->see($user->name);
     }
+
+    public function test_old_urls_are_redirected()
+    {
+      // Having
+      $user = $this->defaultUser();
+
+      $post = factory(\App\Post::class)->make([
+        'title' => 'Old title',
+      ]);
+
+      $user->posts()->save($post);
+
+      $url = $post->url;
+
+      $post->update(['title' => 'New title']);
+
+      $this->visit($url)
+          ->seePageIs($post->url);
+    }
+/*
+    public function test_post_url_with_wrong_slugs_still_works()
+    {
+      // Having
+      $user = $this->defaultUser();
+
+      $post = factory(\App\Post::class)->make([
+        'title' => 'Old title',
+      ]);
+
+      $user->posts()->save($post);
+
+      $url = $post->url;
+
+      $post->update(['title' => 'New title']);
+
+      $this->get($url)
+          ->assertResponseOk()
+          ->see('New title');
+
+    }
+    */
 }
