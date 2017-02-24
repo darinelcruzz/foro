88f495316c2c7074b65a1a05d8f66e8a1f171877
Darinel Cruz <darinelcruzz@gmail.com>
2017-02-24 13:13:22 -0600

    Validacion de politicas de acceso con pruebas de regresion

--- tests/features/AcceptAnswerTest.php
+++ tests/features/AcceptAnswerTest.php
@@ -22,4 +22,47 @@ class AcceptAnswerTest extends FeatureTestCase
     $this->seePageIs($comment->post->url)
         ->seeInElement('.answer', $comment->comment);
   }
+
+  public function test_non_posts_author_dont_see_the_accept_answer_button()
+  {
+    $comment = factory(\App\Comment::class)->create([
+      'comment' => 'Esta va a ser la respuesta del post'
+    ]);
+
+    $this->actingAs(factory(\App\User::class)->create());
+
+    $this->visit($comment->post->url)
+        ->dontSee('Aceptar respuesta');
+  }
+
+  public function test_non_posts_author_cannot_accept_a_comment_as_the_posts_answer()
+  {
+    $comment = factory(\App\Comment::class)->create([
+      'comment' => 'Esta va a ser la respuesta del post'
+    ]);
+
+    $this->actingAs(factory(\App\User::class)->create());
+
+    $this->post(route('comments.accept', $comment));
+
+    $this->seeInDatabase('posts', [
+      'id' => $comment->post_id,
+      'pending' => true,
+    ]);
+  }
+
+  public function test_the_accept_button_is_hidden_when_the_comment_is_already_the_posts_answer()
+  {
+    $comment = factory(\App\Comment::class)->create([
+      'comment' => 'Esta va a ser la respuesta del post'
+    ]);
+
+    $this->actingAs($comment->post->user);
+
+    $comment->markAsAnswer();
+
+    $this->visit($comment->post->url)
+        ->dontSee('Aceptar respuesta');
+  }
+
 }
