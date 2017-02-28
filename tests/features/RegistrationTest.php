64142914dad41151524cdfbc4a67f6b70bf72945
Darinel Cruz <darinelcruzz@gmail.com>
2017-02-28 09:26:17 -0600

    Registro con token y sin contraseña en Laravel

---
+++ tests/features/RegistrationTest.php
@@ -0,0 +1,52 @@
+<?php
+
+use App\User;
+use App\Token;
+use App\Mail\TokenMail;
+use Illuminate\Support\Facades\Mail;
+
+class RegistrationTest extends FeatureTestCase
+{
+  function test_a_user_can_create_an_account()
+  {
+    Mail::fake();
+
+    $this->visitRoute('register')
+        ->type('darinelcruzz@gmail.com', 'email')
+        ->type('darinelcruzz', 'username')
+        ->type('Darinel', 'first_name')
+        ->type('Cruz', 'last_name')
+        ->press('Regístrate');
+
+    $this->seeInDatabase('users', [
+      'email' => 'darinelcruzz@gmail.com',
+      'username' => 'darinelcruzz',
+      'first_name' => 'Darinel',
+      'last_name' => 'Cruz',
+    ]);
+
+    $user = User::first();
+
+    $this->seeInDatabase('tokens', [
+      'user_id' => $user->id
+    ]);
+
+    $token = Token::where('user_id', $user->id)->first();
+
+    $this->assertNotNull($token);
+
+    Mail::assertSentTo($user, TokenMail::class, function ($mail) use ($token) {
+      return $mail->token->id == $token->id;
+    });
+
+    /**
+    * @todo: finish this feature!
+    */
+
+    return;
+    
+    $this->seeRouteIs('register_confirmation')
+        ->see('Gracias por registrarte')
+        ->see('Envíamos a tu email un enlace para que inicies sesión');
+  }
+}
